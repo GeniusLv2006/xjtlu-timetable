a9954cd58e4def850e3c32cd9a4d83816aa72034
GeniusLv2006 <tingkailyu@icloud.com>
2026-03-16 00:25:01 +0800

    fix: 字体改为自托管，移除 Google Fonts 外部请求

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,9 @@
+// Self-hosted fonts (replaces Google Fonts)
+import '@fontsource/noto-sans-sc/400.css'
+import '@fontsource/noto-sans-sc/500.css'
+import '@fontsource/noto-sans-sc/700.css'
+import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/ibm-plex-mono/500.css'
 import './style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
